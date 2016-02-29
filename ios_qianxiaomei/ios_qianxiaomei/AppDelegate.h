2c8d79739c88f4290042f2591f8b2ef9112fb074
qianxiaomei <839505025@qq.com>
2016-02-29 20:51:24 +0800

    qianxiaomei

---
+++ ios_qianxiaomei/ios_qianxiaomei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_qianxiaomei
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 12计科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_qianxiaomei/ios_qianxiaomei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_qianxiaomei
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 12计科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
