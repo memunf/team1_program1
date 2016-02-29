1f4a588cad8e08af38eb13056bd00fb9efeb70c5
qianxiaomei <839505025@qq.com>
2016-02-29 19:46:56 +0800

    srghrthhnet

---
+++ TestDemo/TestDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 12计科. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
